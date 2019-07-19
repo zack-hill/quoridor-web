a78621ab87341959736f4e7c05c3ed47759cd6ea
Zack Hill <zackary.t.hill@gmail.com>
2019-07-19 17:20:53 -0400

    Add ability to pause the game

--- script.js
+++ script.js
@@ -1,12 +1,8 @@
 var canvas;
 var turnDelay;
 var game;
-
-function resizeCanvas() {
-    canvas.width = canvas.clientWidth;
-    canvas.height = canvas.clientWidth;
-    game.draw(canvas);
-}
+var isPlaying;
+var cancelToken = new Object();
 
 function draw() {
     let showDebugMatrix = document.getElementById("dm-distance-matrix").checked;
@@ -14,10 +10,16 @@ function draw() {
     game.draw(canvas, showDebugMatrix, debugMatrixPlayerIndex);
 }
 
+function resizeCanvas() {
+    canvas.width = canvas.clientWidth;
+    canvas.height = canvas.clientWidth;
+    draw();
+}
+
 function onLoad() {
     canvas = document.getElementById("board");
     turnDelay = document.getElementById("turnDelayValue").value;
-    
+
     window.addEventListener("resize", resizeCanvas, false);
 
     let player1 = new Player();
@@ -26,17 +28,10 @@ function onLoad() {
     draw()
 }
 
-function sleep() {
-    if(turnDelay == 0) {
-        return;
-    }
-    return new Promise(resolve => setTimeout(resolve, turnDelay));
-}
-
 function insertTurnRow(turnNumber, message) {
     let turnTableBody = document.getElementById("turn-table-body");
     let row = turnTableBody.insertRow();
-    
+
     var div = document.createElement("div");
     div.className = turnNumber % 2 == 1 ? "player-chip-1" : "player-chip-2";
 
@@ -45,24 +40,52 @@ function insertTurnRow(turnNumber, message) {
     row.insertCell(2).innerHTML = message;
 }
 
-async function onPlay() {
+async function gameLoop(cancelToken) {
     while (game.winningPlayerIndex == -1) {
         let turn = game.takeTurn();
 
         insertTurnRow(game.turns.length - 1, turn.action.toString());
 
         draw()
-        await sleep();
+
+        if (turnDelay != 0) {
+            await new Promise(resolve => setTimeout(resolve, turnDelay));
+        }
+        if (cancelToken.isCancelled) {
+            return;
+        }
     }
     console.log("Player " + (game.winningPlayerIndex + 1) + " Wins!");
 }
 
+function setIsPlaying(value) {
+    isPlaying = value;
+    document.getElementById("btn-play").hidden = value;
+    document.getElementById("btn-pause").hidden = !value;
+    if (value) {
+        cancelToken = new Object();
+        cancelToken.isCancelled = false;
+        gameLoop(cancelToken);
+    } else {
+        cancelToken.isCancelled = true;
+    }
+}
+
+function onPlay() {
+    setIsPlaying(true);
+}
+
+function onPause() {
+    setIsPlaying(false);
+}
+
 function onReset() {
+    setIsPlaying(false);
     game.reset();
-    game.draw(canvas);
+    draw();
     let turnTableBody = document.getElementById("turn-table-body");
     while (turnTableBody.hasChildNodes) {
-        turnTableBody.removeChild(turnTableBody.lastChild,);
+        turnTableBody.removeChild(turnTableBody.lastChild);
     }
 }
 
